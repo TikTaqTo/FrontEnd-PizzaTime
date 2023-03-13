52c028b48a477db11d657a45ac03cc78af3d9ca9
ezhekhan.adai <ezhekhan.adai@alser.kz>
2023-03-13 11:38:23 +0600

    Сделал базовую версию которая обращается на mock сервер

---
+++ src/redux/pizza/asyncActions.ts
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { Pizza, SearchPizzaParams } from './types';
+import pickBy from 'lodash/pickBy';
+import identity from 'lodash/identity';
+
+export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
+  'pizza/fetchPizzasStatus',
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    console.log(params, 4444);
+    const { data } = await axios.get<Pizza[]>(`https://640e9d3b4ed25579dc380f72.mockapi.io/api/v1/Items`, {
+      params: pickBy(
+        {
+          page: currentPage,
+          limit: 4,
+          category,
+          sortBy,
+          order,
+          search,
+        },
+        identity,
+      ),
+    });
+
+    return data;
+  },
+);
